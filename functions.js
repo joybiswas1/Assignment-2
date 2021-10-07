8050821b06a582e214a9dd4ba6796c4b355051d5
Mashroor Ali <alimashroor1@gmail.com>
2021-10-07 13:51:59 -0400

    added the js file

---
+++ functions.js
@@ -0,0 +1,59 @@
+// FOR EACH //
+Array.prototype.myEach = function() {
+
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// FILTER //
+Array.prototype.myFilter = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// EVERY //
+Array.prototype.myEvery = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH //
+Array.prototype.myPush = function() {
+
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.grabKeys = function() {
+
+};
+
+// VALUES //
+Object.grabValues = function() {
+
+};
