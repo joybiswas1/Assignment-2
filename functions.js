4ad53d350e14f8eb1c12485f2aa46a8f363d4b2f
Mashroor Ali <alimashroor1@gmail.com>
2021-10-07 14:38:57 -0400

    added myMap to the functions

--- functions.js
+++ functions.js
@@ -1,9 +1,6 @@
 // FOR EACH //
-Array.prototype.myEach = function(callbackFn) {
-    for(let i =0; i < this.length; i++){
-        if(this[i] == undefined) continue;
-        callbackFn(this[i], i, this);
-    }
+Array.prototype.myEach = function() {
+
 };
 
 // MAP //
